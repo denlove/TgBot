a700e0df03c601784278a541706e2342e328f96b
denlove <bronzovdenis59@gmail.com>
2024-01-08 00:46:49 +0300

    TB-1: first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Bot } from 'grammy'
+
+const main = () => {
+    const token = process.env.TOKEN || ''
+
+    const bot = new Bot(token)
+
+    bot.command('start', ctx => ctx.reply('Welcome! Up and running.'))
+
+    bot.on('message', ctx => ctx.reply('Got another message!'))
+
+    // Start the bot.
+    bot.start()
+}
+
+main()
